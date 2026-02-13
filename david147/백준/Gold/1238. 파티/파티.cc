0535872ac7a47fe65c48e9f8f1293397efc7efd7
FaceWithNull <dooopro00@gmail.com>
2026-02-13 22:22:28 +0900

    Boj/gold/1238 (#13)
    * [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2028 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold V] Title: 파이프 옮기기 1, Time: 140 ms, Memory: 38288 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold IV] Title: 트리의 지름, Time: 0 ms, Memory: 2788 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold II] Title: 트리의 지름, Time: 88 ms, Memory: 10616 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2032 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold IV] Title: 최단경로, Time: 72 ms, Memory: 9184 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold IV] Title: 별 찍기 - 11, Time: 132 ms, Memory: 20760 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    * [Gold III] Title: 파티, Time: 616 ms, Memory: 5928 KB -BaekjoonHub
    
    * auto: merge baekjoon files into david147 folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ david147/백준/Gold/1238. 파티/파티.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <climits>
+#include <queue>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define INF 1 << 24
+
+int n, m, h, maximum = INT_MIN, minimum = INT_MAX, ans;
+int cost[1000][1000];
+
+void f() {
+
+}
+
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m >> h;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) cost[i][j] = INF;
+        }
+    }
+
+    int start, end, dist;
+    for (int i = 0; i < m; i++) {
+        cin >> start >> end >> dist;
+        if (dist < cost[start - 1][end - 1]) cost[start - 1][end - 1] = dist;
+    }
+
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (cost[i][j] > cost[i][k] + cost[k][j]) cost[i][j] = cost[i][k] + cost[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (maximum < cost[i][h - 1] + cost[h - 1][i]) maximum = cost[i][h - 1] + cost[h - 1][i];
+    }
+    
+    cout << maximum << '\n';
+    
+    return 0;
+}
