8d651f122bb37be24c173fc961aef8db76219e68
Sungjae Park <98754301+sungjaep11@users.noreply.github.com>
2026-03-07 13:06:14 +0900

    Boj/gold/1766 (#237)
    * [Gold II] Title: 문제집, Time: 28 ms, Memory: 3944 KB -BaekjoonHub
    
    * auto: merge solved problems into hankookin folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ hankookin/백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> adj(N+1);
+    vector<int> indegree(N+1, 0);
+
+    for (int i=0; i<M; i++) {
+        int A, B;
+        cin >> A >> B;
+
+        adj[A].push_back(B);
+        indegree[B]++;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i=1; i<=N; i++) {
+        if (indegree[i] == 0) {
+            pq.push(i);
+        }
+    }
+
+    while (!pq.empty()) {
+        int curr = pq.top();
+        pq.pop();
+        cout << curr << " ";
+
+        for (int next : adj[curr]) {
+            indegree[next]--;
+            if (indegree[next] == 0) {
+                pq.push(next);
+            }
+        }
+    }
+
+    return 0;
+}
