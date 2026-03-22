5e9337b94df6873c1a280704e2fb7a3f031a2314
Yeongun Choi <dooopro00@gmail.com>
2026-03-22 20:19:39 +0900

    Boj/silver/24444 (#485)
    * [Gold IV] Title: 이분 그래프, Time: 212 ms, Memory: 11376 KB -BaekjoonHub
    
    * auto: merge solved problems into david147 folder
    
    * [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 92 ms, Memory: 10932 KB -BaekjoonHub
    
    * auto: merge solved problems into david147 folder
    
    * [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 80 ms, Memory: 9552 KB -BaekjoonHub
    
    * auto: merge solved problems into david147 folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ david147/백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF (1 << 28)
+#define N (int) 1e5
+#define ll long long
+
+int n, m, h, maximum = INT_MIN, minimum = INT_MAX, ans;
+bool flag;
+int idx;
+vector<vector<int>> adj(N + 1);
+vector<int> order(N + 1, 0);
+
+void f(int node) {
+    queue<int> q;
+    q.push(node);
+
+    int cur;
+    while (!q.empty()) {
+        cur = q.front();
+        q.pop();
+
+        if (order[cur]) continue;
+        order[cur] = ++idx;
+
+        for (const int& elt : adj[cur]) {
+            if (!order[elt]) q.push(elt);
+        }
+    }
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> m >> h;
+
+    int start, end;
+    for (int i = 0; i < m; ++i) {
+        cin >> start >> end;
+        adj[start].push_back(end);
+        adj[end].push_back(start);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        sort(adj[i].begin(), adj[i].end());
+    }
+
+    f(h);
+
+    for (int i = 1; i <= n; ++i) cout << order[i] << '\n';
+
+    return 0;
+}
