aeaa9e7f45b4790eed67fffedb979a32f13dd8ab
KangYeSeo04 <e64888283@gmail.com>
2026-03-22 17:48:36 +0900

    Boj/silver/1927 (#480)
    * boj/Bronze/14467
    
    * solved boj/Bronze/21918
    
    * solved bog/Silver/20546
    
    * solved boj/Silver/7568
    
    * solved boj/Silver/10709
    
    * sovled boj/Silver/9017
    
    * solved soj/Silver/2852
    
    * solved boj/Silver/1913
    
    * solved boj/Silver/20006
    
    * solved soj/Silver/1138
    
    * solved boj/Silver/17276
    
    * solved boj/Silver/16926
    
    * solved boj/Gold/14719
    
    * solved boj/Gold/20207
    
    * solved boj/Gold/22860
    
    * solved boj/Silver/10828
    
    * solved boj/Silver/10845
    
    * solved boj/Silver/3986
    
    * solved boj/Silver/2164
    
    * solved boj/Silver/9375
    
    * solved boj/Silver/1874
    
    * [Silver II] Title: 스택 수열, Time: 116 ms, Memory: 39268 KB -BaekjoonHub
    
    * solved boj/Silver/14425
    
    * solved boj/Silver/2910
    
    * solved boj/Silver/1927

---
+++ kangyeseo0724/BaekJoon/Silver/1927최소힙.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+
+heap = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    
+    if x > 0:
+        heapq.heappush(heap, x)
+    else:
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
